49d4a1b4b9f4d4ec1d21221fc88cb91e1e98dcff
AJ Ziarek <deltrigger@pm.me>
2021-04-03 21:17:50 -0500

    js work, save and load tasks

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,2 +1,27 @@
+// displaying date per documentation for moment.js
 const currentTime = moment();
 $('#currentDay').text(currentTime.format('MMMM Do, YYYY'));
+
+
+// we need an object to store the tasks
+var tasks = {
+    '0900': [],
+    '1000': [],
+    '1100': [],
+    '1200': [],
+    '1300': [],
+    '1400': [],
+    '1500': [],
+    '1600': [],
+    '1700': []
+};
+
+// we need a function to setItem to localStorage
+var saveTasks = function() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// now to we need to retrieve the tasks by parsing
+var loadTasks = function() {
+    JSON.parse(localStorage.getItem('tasks'));
+}
