bd53d22a6332b0ed5b16644378a38f16a34be4ed
AJ Ziarek <deltrigger@pm.me>
2021-04-04 19:37:34 -0500

    working class changer based on time of day

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,37 +1,44 @@
 // displaying date per documentation for moment.js
 const currentTime = moment();
-$('#currentDay').text(currentTime.format('MMMM Do, YYYY'));
+$('#currentDay').text(currentTime.format('LLLL'));
 
 
 // we need an object to store the tasks
-var tasks = {
-    '0900': [],
-    '1000': [],
-    '1100': [],
-    '1200': [],
-    '1300': [],
-    '1400': [],
-    '1500': [],
-    '1600': [],
-    '1700': []
+const tasks = {
+    '9': [],
+    '10': [],
+    '11': [],
+    '12': [],
+    '13': [],
+    '14': [],
+    '15': [],
+    '16': [],
+    '17': []
 };
 
+// event listener that grabs siblings of all 'row' text and stores
+$('.saveBtn').on('click', function() {
+    let taskText = $(this).siblings('.row').val();
+    var time = $(this).parent().attr("id");
+})
+
 // we need a function to setItem to localStorage
-var saveTasks = function() {
+const saveTasks = function() {
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // now to we need to retrieve the tasks by parsing
-var loadTasks = function() {
+const loadTasks = function() {
     JSON.parse(localStorage.getItem('tasks'));
 }
+
 
 // handling time by removing/adding classes for past, present, future
 
 function colorCoder() {
     let checkTime = moment().hour();
     $('.row').each(function() {
-        let auditTime = parseInt($(this).attr("id").split("hour")[1]);
+        let auditTime = parseInt($(this).attr("id"));
 
         if (auditTime < checkTime) {
             $(this).removeClass("future");
@@ -54,4 +61,5 @@ function colorCoder() {
 
 }
 
+loadTasks();
 colorCoder();
